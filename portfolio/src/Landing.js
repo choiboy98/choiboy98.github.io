511ede84f696d15c01366bb691c0b941ff17eee2
choiboy98 <daniel.won.choi@gmail.com>
2019-09-13 20:04:43 -0500

    animation

--- portfolio/src/Landing.js
+++ portfolio/src/Landing.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react'
 import daniel from './static/image/daniel.png'
 import food from './static/image/food.png'
 import music from './static/image/music.png'
@@ -9,37 +9,52 @@ import github from './static/image/github.png'
 import './style/landing.css'
 import './index.css'
 
-function Landing() {
-  return (
-    <div className="background">
-        <div className="row">
-            <img src={daniel} className="daniel" alt="logo" />
-            <div style={{paddingRight: "25vh"}}>
-                <p className="text" style={{paddingTop: "15vh",fontSize: "3vh"}}>My Name is</p>
-                <p className="text" style={{fontSize: "10vh"}}>Daniel Choi</p>
-                <p className="text" style={{fontSize: "2vh"}}>I am studying Computer Science at the University of Illinois at Urbana-Champaign. I am experienced in Full-stack development, AI, and security. I love to code, sing, and cook!</p>
-                <a>
-                    <img src={project} className="logo" alt="logo" />
-                </a>
-                <a className="logo-animation"href="https://github.com/choiboy98" target="_blank">
-                    <img src={github} className="logo"alt="logo" />
-                </a>
-                <a>
-                <img src={linkedin} className="logo"alt="logo" />
-                </a>
-                <a>
-                <img src={resume} className="logo"alt="logo" />
-                </a>
-                <a>
-                <img src={music} className="logo"alt="logo" />
-                </a>
-                <a>
-                <img src={food} className="logo"alt="logo" />
-                </a>
-            </div>
-        </div>
-    </div>
-  );
-}
+export default class Landing extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+          landing: true,
+          project: false,
+          music: false,
+          food: false,
+        }
+    }
 
-export default Landing;
+    render() {
+        return (
+            <div className="flip-container">
+                <div className="flipper">
+                    <div className="background front">
+                        <div className="row">
+                            <img src={daniel} className="daniel" alt="logo" />
+                            <div style={{paddingRight: "25vh"}}>
+                                <p className="text" style={{paddingTop: "15vh",fontSize: "3vh"}}>My Name is</p>
+                                <p className="text" style={{fontSize: "10vh"}}>Daniel Choi</p>
+                                <p className="text" style={{fontSize: "2vh"}}>I am studying Computer Science at the University of Illinois at Urbana-Champaign. I am experienced in Full-stack development, AI, and security. I love to code, sing, and cook!</p>
+                                <a href="https://github.com/choiboy98" target="_blank">
+                                    <img src={project} className="logo logo-animation" alt="logo" />
+                                </a>
+                                <a href="https://github.com/choiboy98" target="_blank">
+                                    <img src={github} className="logo logo-animation" alt="logo" />
+                                </a>
+                                <a href="https://www.linkedin.com/in/choiboy98/" target="_blank">
+                                <img src={linkedin} className="logo logo-animation" alt="logo" />
+                                </a>
+                                <a href="https://github.com/choiboy98" target="_blank">
+                                <img src={resume} className="logo logo-animation" alt="logo" />
+                                </a>
+                                <a href="https://github.com/choiboy98" target="_blank">
+                                <img src={music} className="logo logo-animation" alt="logo" />
+                                </a>
+                                <a href="https://github.com/choiboy98" target="_blank">
+                                <img src={food} className="logo logo-animation" alt="logo" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          );
+    }
+}
